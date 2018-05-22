b92e59c74ad9f74e6acb647c6442b5b7033ba95b
am468y <am468y@att.com>
2018-05-22 17:37:02 +0530

    fix bodyparser json

--- socket.js
+++ socket.js
@@ -1,23 +1,50 @@
-let express = require('express');
-let http = require('http');
+const express = require('express');
+const http = require('http');
+const bodyParser = require('body-parser');
+
 let app = express();
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json());
 
 let server = http.createServer(app);
 let io = require('socket.io')(server);
 
-io.on('connection', function(client){
+let data = "";
+
+io.on('connection', function (client) {
   console.log('A client connected');
+  let counter = 0;
+  setInterval(() => {
+    if(data) {
+      client.emit('message', data);
+      data = "";
+    }    
+  },0);
+
+
 });
 
-app.get('/', function(req, res){
-    res
-        .status(200)
-        .send('this is my lovely socket program');
+
+
+app.get('/', function (req, res) {
+  res
+    .status(200)
+    .send('this is my lovely socket program');
 });
 
+app.post('/message', function (req, res) {
+  // console.log(req.headers);
+  // console.log(req.body);
+  data = req.body.message;
+  console.log(data);
+  res
+    .status(200)
+    .send({'message': 'message received'});
+});
 
 let port = 8181;
-server.listen(port, function(){
-    console.log('listening on port : ' + port);
+server.listen(port, function () {
+  console.log('listening on port : ' + port);
 });
-
